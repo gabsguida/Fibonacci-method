b5ef16256fefdeaa74b3f5cf980def4ab5d2938e
Gabriela Guida <gabrielaguida7@gmail.com>
2020-04-21 18:04:19 -0300

    index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const fibonacciUntil = (number) => {
+    let arr = [0,1];
+    let sum = 0;
+    while (sum < number){
+        sum = arr[arr.length - 2] + arr[arr.length - 1];
+        arr.push(sum);
+    }
+    return arr;
+}
+
+
+const fibonacci = (number=350) => {
+    return fibonacciUntil(350);    
+}
+
+const isFibonnaci = (num) => {
+    let arrFibonacci = fibonacci();
+    return arrFibonacci.includes(num);
+}
+
+module.exports = {
+    fibonacci,
+    isFibonnaci
+}
